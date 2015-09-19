3048a14853e346dbddcdf45c320d5b0d69571b72
hjgu <hjgu@iFlytek.com>
2015-09-19 19:16:54 -0400

    merged UI stuff with Angular stuff

--- src/main/webapp/public/js/app.js
+++ src/main/webapp/public/js/app.js
@@ -1,9 +1,29 @@
 'use strict';
+var app = angular.module('myApp', []);
 
-var app = angular.module('myApp',[]);
+var User = function() {
+    this.session = null;
+    this.email = null;
+    this.password = null;
+};
 
-app.controller('LoginController', ['$scope', function($scope) {
-    $scope.buttonClick = function() {
-        console.log("hi");
+app.service('LoginService', ['$http', function($http){
+    this.login = function(student) {
+        console.log("test");
+        return $http.post('/student/login', {email:student.email, password: student.password});
     };
+}]);
+
+app.controller('LoginController', ['$scope', 'LoginService', function ($scope, LoginService) {
+    $scope.greeting = 'Hola!';
+    $scope.user = new User();
+    $scope.login = function () {
+        var promise = LoginService.login($scope.user);
+        promise.then(function (response) {
+            $scope.user.session = response.data.session;
+
+        }, function (response) {
+            console.log(response);
+        });
+    }
 }]);
