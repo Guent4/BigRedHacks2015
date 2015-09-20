ba22c9a083acb1a5c56db6804ef441c8607ba33f
Rohoe <rh535@cornell.edu>
2015-09-19 22:59:19 -0400

    signup page and login home page

--- src/main/webapp/public/js/app.js
+++ src/main/webapp/public/js/app.js
@@ -27,21 +27,3 @@ app.controller('LoginController', ['$scope', 'LoginService', function ($scope, L
         });
     }
 }]);
-
-function CarouselDemoCtrl($scope){
-  $scope.myInterval = 3000;
-  $scope.slides = [
-    {
-      image: 'http://lorempixel.com/400/200/'
-    },
-    {
-      image: 'http://lorempixel.com/400/200/food'
-    },
-    {
-      image: 'http://lorempixel.com/400/200/sports'
-    },
-    {
-      image: 'http://lorempixel.com/400/200/people'
-    }
-  ];
-}
